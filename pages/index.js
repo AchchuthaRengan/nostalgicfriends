06c6cb9e568b21905378467d575662414d068025
dimndot <93150262+dimndot@users.noreply.github.com>
2023-02-26 09:49:03 +0530

    Main Home Page Rendered with links and 100% responsive

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Header from "@/components/Header";
-import NavBar from "../components/NavBar";
+import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
     <>
       <main>
         <div>
           <Header />
-          <NavBar />
+          <Hero/>
         </div>
       </main>
     </>
